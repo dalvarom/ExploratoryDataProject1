220d7f46be05777c72079ff930227ccac3ab307d
dalvarom <dalvarom@gmail.com>
2014-07-13 10:12:26 +0200

    Plot 1

---
+++ plot 1.R
@@ -0,0 +1,120 @@
+# Introduction
+# 
+# This assignment uses data from the UC Irvine Machine Learning Repository, a popular repository for machine 
+# learning datasets. In particular, we will be using the "Individual household electric power consumption Data 
+# Set" which I have made available on the course web site:
+#  
+# .Dataset: Electric power consumption [20Mb]
+#
+# .Description: Measurements of electric power consumption in one household with a one-minute sampling rate over 
+# a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
+#
+#The following descriptions of the 9 variables in the dataset are taken from the UCI web site:
+#1.Date: Date in format dd/mm/yyyy 
+#2.Time: time in format hh:mm:ss 
+#3.Global_active_power: household global minute-averaged active power (in kilowatt) 
+#4.Global_reactive_power: household global minute-averaged reactive power (in kilowatt) 
+#5.Voltage: minute-averaged voltage (in volt) 
+#6.Global_intensity: household global minute-averaged current intensity (in ampere) 
+#7.Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, 
+#  containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered). 
+#8.Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry 
+#  room, containing a washing-machine, a tumble-drier, a refrigerator and a light. 
+#9.Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric 
+# water-heater and an air-conditioner.
+#
+#Loading the data
+#
+#When loading the dataset into R, please consider the following:
+#  
+# The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much memory the dataset
+# will require in memory before reading into R. Make sure your computer has enough memory (most modern computers 
+# should be fine).
+#
+# We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from 
+# just those dates rather than reading in the entire dataset and subsetting to those dates.
+#
+#
+# You may find it useful to convert the Date and Time variables to Date/Time classes in R using the strptime() 
+# and as.Date() functions.
+# 
+# Note that in this dataset missing values are coded as ?.
+#
+# Making Plots
+# 
+# Our overall goal here is simply to examine how household energy usage varies over a 2-day period in February, 
+# 2007. Your task is to reconstruct the following plots below, all of which were constructed using the base 
+# plotting system.
+# 
+# First you will need to fork and clone the following GitHub repository: https://github.com/rdpeng/ExData_Plotting1
+#
+# For each plot you should
+# 
+# .Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+# .Name each of the plot files as plot1.png, plot2.png, etc.
+# .Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the corresponding plot, i.e. code in 
+# plot1.R constructs the plot1.png plot. Your code file should include code for reading the data so that the plot 
+# can be fully reproduced. You should also include the code that creates the PNG file.
+# .Add the PNG file and R code file to your git repository
+#
+# When you are finished with the assignment, push your git repository to GitHub so that the GitHub version of your 
+# repository is up to date. There should be four PNG files and four R code files.
+# The four plots that you will need to construct are shown below. 
+
+# Select your working directory
+setwd("D:/Trabajo/Cursos/Exploratory data analysis/project 1")
+
+# Create a folder to save the zip file on it
+if (!file.exists("./data")) {dir.create("./data")}
+zipUrl= "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zipFile = "./data/data.zip"
+download.file (zipUrl, destfile=zipFile, method="auto")
+
+# Unzip the file
+unzip (zipFile)
+
+# After that you will have a txt file called household_power_consumption.txt
+textFile = "household_power_consumption.txt";
+consumptionData = read.csv (textFile,header=F, sep=";");
+
+# Take a look at the first rows of the file
+head (consumptionData);
+
+# Assign names to each column
+arr_cols <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", 
+              "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+for (i in 1:length(arr_cols)){
+  names (consumptionData)[i] <-arr_cols[i]
+}
+
+# Load the package
+# install.packages("sqldf", repos="http://cran.us.r-project.org", dependencies=TRUE)
+library(sqldf)
+
+# We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from 
+# just those dates rather than reading in the entire dataset and subsetting to those dates.
+dataFrame<-sqldf("select * from consumptionData where Date = '1/2/2007' or Date = '2/2/2007'") 
+
+# If you make the following sentence, you'll see that there are NA elements on the subset
+str(dataFrame)
+
+# So we remove them
+aux <-  dataFrame['Global_active_power'][!is.na( dataFrame['Global_active_power'])]
+
+# And now there is no error
+str (aux)
+
+# So we can make the histogram
+aux <- sapply(aux, as.numeric)
+hist(aux, col="red", main ="Global Active Power",xlab="Global Active Power (kilowatts)")
+
+#Finally we save the plot as PDF
+pdf (file="myplot.png")
+hist(aux, col="red", main ="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
+
+# Or save it as PNG as asked on the project
+png(file="plot1.png",width=480,height=480)
+hist(aux, col="red", main ="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
